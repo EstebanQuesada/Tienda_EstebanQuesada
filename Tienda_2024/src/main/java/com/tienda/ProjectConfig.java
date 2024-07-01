c7a837cbf5ae3dc7e915798c1baa26b02a658335
EstebanQuesada <equesada00666@ufide.ac.cr>
2024-06-30 21:33:46 -0600

    Semana 6

--- Tienda_2024/src/main/java/com/tienda/ProjectConfig.java
+++ Tienda_2024/src/main/java/com/tienda/ProjectConfig.java
@@ -3,48 +3,62 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.tienda;
-
 import java.util.Locale;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.web.servlet.LocaleResolver;
-import org. springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet. config. annotation. WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-/**
- *
- * @author EsteP
- */
 
-/**
-*
-* @author cesar
+
+public class ProjectConfig {
+/*
+    La clase implementa la interfaz por lo cual va a llevar metodos especificos de esa clase 
+    lo que llevaria a los diferentes Beans
 */
-@Configuration
-public class ProjectConfig implements WebMvcConfigurer {
-/* Los siguientes métodos son para incorporar el tema de internacionalización en el proyecto
-/* localeResolver se utiliza para crear una sesión de cambio de idioma*/
+public class ProjectConfig implements WebMvcConfigurer{
+    /*
+    Un @Bean son notaciones de spring que funcionan para definir metodos y seran 
+    gestionados por el contenedor de Spring 
+    
+    El localeResolver se utiliza para sesion de usuario y configuracion regional
+    */
+     @Bean
+    public SessionLocaleResolver localeResolver() {
+        var slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.getDefault());
+        slr.setLocaleAttributeName("session.current.locale");
+        slr.setTimeZoneAttributeName("session.current.timezone");
 
-@Bean
-public LocaleResolver localeResolver() {
-var slr = new SessionLocaleResolver();
-slr.setDefaultLocale(Locale.getDefault ());
-slr.setLocaleAttributeName("session.current.locale");
-slr. setTimeZoneAttributeName("session.current. timezone");
-return slr;
-}  
+        return slr;
+    }
+    /*
+       Este funciona para cambiar el locale en funcion de una solicitud osea modifica el de arriba
+    */
+    @Bean
+    public LocaleChangeInterceptor localeChanceInterceptor(){
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+    /*
+    Este no lo entendi 
+    */
+    @Override
+    public void addInterceptors(InterceptorRegistry registro){
+        registro.addInterceptor(localeChanceInterceptor());
+    }
+    /*
+        Este lee archivps y establece nombres base de los archivos de mensajes 
+    */
+    @Bean("messageBrouce")
+    public MessageSource messageSource(){
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("message");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
 
-@Bean
-public LocaleChangeInterceptor localeChangeInterceptor () {
-var lci = new LocaleChangeInterceptor();
-lci.setParamName("lang");
-return lci;
-}  
-
-public void addInterceptor(InterceptorRegistry registro) {
-    registro.addInterceptor(localeChangeInterceptor());
-}
 }
